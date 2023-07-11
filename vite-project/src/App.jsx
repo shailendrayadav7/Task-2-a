1df4ebab49ce8f62ac41ee98df044c125614be2d
shailendrayadav7 <sailendrayadav80@gmail.com>
2023-07-11 14:46:00 -0500

    Display the expected output

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,30 +4,43 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const students = [
+    { name: "Samantha", age: 38, grades: [80,85,90] },
+    { name: "Riley", age: 24, grades: [75, 85, 95] },
+    {name: "Dall-E", age: 22, grades: [91, 92, 93] }
+  ];
+
+  function calculateAverageGrade(student){
+    let sum = 0;
+    const grades = student.grades;
+
+    for (let i = 0; i < grades.length; i++){
+      sum += grades[i];
+    }
+    return sum / grades.length;
+  }
+
+  function printStudentInfo(student){
+    console.log('Student Name: ', student.name);
+    console.log('Student Age: ', student.age);
+    console.log('Average Grade: ', calculateAverageGrade(student));
+  }
+
+  printStudentInfo(students[1])
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       <h1>Student Information</h1>
+       {students.map((student, index) => (
+        <div key={index}>
+          <h2>{student.name}</h2>
+          <p>Age: {student.age}</p>
+          <p>Average Grade: {calculateAverageGrade(student)}</p>
+          </div>
+       ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
